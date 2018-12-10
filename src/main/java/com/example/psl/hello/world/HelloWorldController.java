2361b48269ed66692701443eb0db111330d55a07
Camilo Tobar <camilo.tobar@outlook.com>
2018-12-10 14:40:46 -0500

    Enviroment variable added

--- src/main/java/com/example/psl/hello/world/HelloWorldController.java
+++ src/main/java/com/example/psl/hello/world/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.psl.hello.world;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    @Value("${HELLO:Hello}")
+    private String greeting;
+
     @GetMapping("/hello")
     public String HelloWorld(){
-        return "Hello World";
+        return greeting + " World";
     }
 }
